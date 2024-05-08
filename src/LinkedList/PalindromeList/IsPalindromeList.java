ed1171bed8590ef573e04ac91ec3154e0e34b42d
Cai Rui <cairui99@outlook.com>
2024-05-09 02:14:07 +0800

    回文链表

---
+++ src/LinkedList/PalindromeList/IsPalindromeList.java
@@ -0,0 +1,18 @@
+package LinkedList.PalindromeList;
+
+import LinkedList.Node;
+
+import java.util.Stack;
+
+public class IsPalindromeList {
+
+    /**
+     * 空间复杂度O(N)
+     */
+    public boolean isPalindromeListN1(Node head) {
+        Stack<Node> nodeStack = new Stack<>();
+
+        return true;
+    }
+
+}
