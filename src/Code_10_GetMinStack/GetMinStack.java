90a47c0fd3d724050c1a44029fc394c6ddf69b83
Cai Rui <cairui99@outlook.com>
2024-05-20 01:45:19 +0800

    弹出最小元素的栈

---
+++ src/Code_10_GetMinStack/GetMinStack.java
@@ -0,0 +1,33 @@
+package Code_10_GetMinStack;
+
+import java.util.Stack;
+
+public class GetMinStack {
+
+    private final Stack<Integer> dataStack;
+    private final Stack<Integer> minStack;
+
+    public GetMinStack() {
+        dataStack = new Stack<>();
+        minStack  = new Stack<>();
+    }
+
+    public void push(int num) {
+        dataStack.push(num);
+        if (!minStack.isEmpty() && minStack.peek() < num) {
+            minStack.push(minStack.peek());
+        } else {
+            minStack.push(num);
+        }
+    }
+
+    public int pop() {
+        minStack.pop();
+        return dataStack.pop();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
